6a3a5d6f90a34a48831033c6ac1324458f26ddd5
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-05-30 17:29:33 +0100

    add testnet data

--- examples/delegation/client/src/config.ts
+++ examples/delegation/client/src/config.ts
@@ -1,5 +1,6 @@
 import { Connection, Ed25519Keypair, JsonRpcProvider, RawSigner } from "@mysten/sui.js";
 
+const fullnode = "https://fullnode.testnet.sui.io:443";
 const ownerMnemonics = "pause estate bargain cycle grow same trumpet north merge check expire zoo";
 const agentMnemonics = "hybrid sniff fan aisle easily seat cycle zoo struggle renew change leave";
 const fakeOwnerMnemonics = "train heavy pizza chunk eight march silver seminar gauge unveil brain upset";
@@ -8,12 +9,12 @@ export const ownerKeypair = Ed25519Keypair.deriveKeypair(ownerMnemonics);
 export const agentKeypair = Ed25519Keypair.deriveKeypair(agentMnemonics);
 export const fakeOwnerKeypair = Ed25519Keypair.deriveKeypair(fakeOwnerMnemonics);
 
-export const provider = new JsonRpcProvider(new Connection({ fullnode: "http://127.0.0.1:9000" }));
+export const provider = new JsonRpcProvider(new Connection({ fullnode }));
 export const ownerSigner = new RawSigner(ownerKeypair, provider);
 export const agentSigner = new RawSigner(agentKeypair, provider);
 export const fakeOwnerSigner = new RawSigner(fakeOwnerKeypair, provider);
 
 export const baseGasBudget = 100_000_000;
-export const babyPackageId = "0x519000a83ffdce37fe0a4a715649e6d02e570ff2a5946f7cd166577138de17fe";
-export const publishReceiptId = "0x419287c04b3e5ea2f5b51132c6d0ad3808e4a62d8a9568ca82ec84d0e79f1959";
-export const ownershipPackageId = "0x67ddd36c005d4ab7f2b5062143f2c33fa8b88e745a9e5d805dd764a06d7675df";
+export const babyPackageId = "0xaacb093dc7c98a79a5441a343fae064ebe6898926c2e07b5922ddae055d9447e";
+export const publishReceiptId = "0xf8f1028144184bf2b894fe4ba5327129eb456bf4510c3e4bafbe98e0021ff7fb";
+export const ownershipPackageId = "0xdcbadd322069f668204440a6245922a705922e9d1e061543c4bbf388d62d92db";
