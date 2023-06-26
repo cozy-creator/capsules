8ed1c917557b92fd3ef570c5eba99da9fa20bd56
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-06-26 12:52:42 +0100

    add removed action functions

--- examples/client_examples/client/src/person.ts
+++ examples/client_examples/client/src/person.ts
@@ -6,6 +6,9 @@ import {
   claimDelegation,
   create as createPerson,
   destroy as destroyPerson,
+  removeActionForObjectsFromAgent,
+  removeActionForTypeFromAgent,
+  removeGeneralActionFromAgent,
   returnAndShare as returnAndSharePerson,
 } from "../ownership/person/functions";
 import { begin as beginTxAuth } from "../ownership/tx-authority/functions";
@@ -144,6 +147,134 @@ async function editBabyWithEmptyAction({ owner, agent }: EditBabyWithAction) {
 
     returnAndSharePerson(txb, person);
     returnAndShareBaby(txb, baby);
+
+    const _tx = await owner.signAndExecuteTransactionBlock({ transactionBlock: txb, options: { showEffects: true } });
+  }
+
+  {
+    const txb = new TransactionBlock();
+    const [auth] = claimDelegation(txb, personId);
+
+    editBabyName(txb, { auth, baby: babyId, newName: "New Baby Name" });
+    const _tx = await agent.signAndExecuteTransactionBlock({ transactionBlock: txb, options: { showEffects: true } });
+  }
+}
+
+async function editBabyWithRemovedGeneralAction({ owner, agent }: EditBabyWithAction) {
+  let personId: string = "",
+    babyId: string = "";
+
+  {
+    const txb = new TransactionBlock();
+    const [person] = createPerson(txb, await owner.getAddress());
+    const [baby] = createBaby(txb, "Initial Baby Name");
+    const [auth] = beginTxAuth(txb);
+
+    addGeneralAction(txb, EDITOR.$typeName, { agent: await agent.getAddress(), auth, person });
+
+    returnAndSharePerson(txb, person);
+    returnAndShareBaby(txb, baby);
+
+    const _tx = await owner.signAndExecuteTransactionBlock({ transactionBlock: txb, options: { showEffects: true } });
+  }
+
+  {
+    const txb = new TransactionBlock();
+    const [auth] = beginTxAuth(txb);
+
+    removeGeneralActionFromAgent(txb, EDITOR.$typeName, { agent: await agent.getAddress(), auth, person: personId });
+    const _tx = await owner.signAndExecuteTransactionBlock({ transactionBlock: txb, options: { showEffects: true } });
+  }
+
+  {
+    const txb = new TransactionBlock();
+    const [auth] = claimDelegation(txb, personId);
+
+    editBabyName(txb, { auth, baby: babyId, newName: "New Baby Name" });
+    const _tx = await agent.signAndExecuteTransactionBlock({ transactionBlock: txb, options: { showEffects: true } });
+  }
+}
+
+async function editBabyWithRemovedTypeAction({ owner, agent }: EditBabyWithAction) {
+  let personId: string = "",
+    babyId: string = "";
+
+  {
+    const txb = new TransactionBlock();
+    const [person] = createPerson(txb, await owner.getAddress());
+    const [baby] = createBaby(txb, "Initial Baby Name");
+    const [auth] = beginTxAuth(txb);
+
+    addActionForType(txb, [CAPSULE_BABY.$typeName, EDITOR.$typeName], {
+      agent: await agent.getAddress(),
+      auth,
+      person,
+    });
+    returnAndSharePerson(txb, person);
+    returnAndShareBaby(txb, baby);
+
+    const _tx = await owner.signAndExecuteTransactionBlock({ transactionBlock: txb, options: { showEffects: true } });
+  }
+
+  {
+    const txb = new TransactionBlock();
+    const [auth] = beginTxAuth(txb);
+
+    removeActionForTypeFromAgent(txb, [CAPSULE_BABY.$typeName, EDITOR.$typeName], {
+      agent: await agent.getAddress(),
+      auth,
+      person: personId,
+    });
+    const _tx = await owner.signAndExecuteTransactionBlock({ transactionBlock: txb, options: { showEffects: true } });
+  }
+
+  {
+    const txb = new TransactionBlock();
+    const [auth] = claimDelegation(txb, personId);
+
+    editBabyName(txb, { auth, baby: babyId, newName: "New Baby Name" });
+    const _tx = await agent.signAndExecuteTransactionBlock({ transactionBlock: txb, options: { showEffects: true } });
+  }
+}
+
+async function editBabyWithRemovedObjectAction({ owner, agent }: EditBabyWithAction) {
+  let personId: string = "",
+    babyId: string = "";
+
+  {
+    const txb = new TransactionBlock();
+    const [person] = createPerson(txb, await owner.getAddress());
+    const [baby] = createBaby(txb, "Initial Baby Name");
+
+    returnAndSharePerson(txb, person);
+    returnAndShareBaby(txb, baby);
+
+    const _tx = await owner.signAndExecuteTransactionBlock({ transactionBlock: txb, options: { showEffects: true } });
+  }
+
+  {
+    const txb = new TransactionBlock();
+    const [auth] = beginTxAuth(txb);
+
+    removeActionForObjectsFromAgent(txb, EDITOR.$typeName, {
+      agent: await agent.getAddress(),
+      auth,
+      person: personId,
+      objects: [babyId],
+    });
+    const _tx = await owner.signAndExecuteTransactionBlock({ transactionBlock: txb, options: { showEffects: true } });
+  }
+
+  {
+    const txb = new TransactionBlock();
+    const [auth] = beginTxAuth(txb);
+
+    addActionForObjects(txb, EDITOR.$typeName, {
+      agent: await agent.getAddress(),
+      auth,
+      person: personId,
+      objects: [babyId],
+    });
 
     const _tx = await owner.signAndExecuteTransactionBlock({ transactionBlock: txb, options: { showEffects: true } });
   }
